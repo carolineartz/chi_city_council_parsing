063152a1aa6d5ec957fe03b58b0ef91985105e27
Caroline Artz <ceartz@gmail.com>
2014-06-15 18:03:58 -0500

    just being silly

--- parser.rb
+++ parser.rb
@@ -1,9 +1,18 @@
-require 'http://rubygems.org'
-require 'json'
+# require 'http://rubygems.org'
+# require 'json'
 # # Dir["/json/*"].each {|file| p require file }
-require 'rolecall1.json'
+# require 'rolecall1.json'
 
-vote_data = File.open("rolecall1.json")
-voting_hash = JSON.parse()
+# vote_data = File.open("rolecall1.json")
+# voting_hash = JSON.parse()
 
-p council_votes
+# p council_votes
+
+
+# def parse
+
+# end
+
+vote_data = File.open("rolecall1.json", "a+")
+
+puts vote_data
