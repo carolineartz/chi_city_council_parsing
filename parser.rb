8c8131817d7b3b67dd497db83bda1e53b2e94723
Justin Stewart <email": null>
2014-06-15 18:11:04 -0500

    chore: too many

---
+++ parser.rb
@@ -0,0 +1,9 @@
+# require 'http://rubygems.org'
+# require 'json'
+# # # Dir["/json/*"].each {|file| p require file }
+# require 'rolecall1.json'
+
+p vote_data = File.open("rolecall1.json", "a+")
+# voting_hash = JSON.parse()
+
+# p council_votes
