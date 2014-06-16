4e0ef645f9f4fee77d1abfc55b4dd59778e1ab7f
Justin Stewart <email": null>
2014-06-16 14:06:30 -0500

    feature: parses most files

--- bestparser.rb
+++ bestparser.rb
@@ -13,15 +13,6 @@ votes4 = JSON.parse(file4)
 votes5 = JSON.parse(file5)
 votes6 = JSON.parse(file6)
 
-# votes["TaggedPDF-doc"]["Part"].each do |key, value|
-
-#   # puts key["Sect"]
-#   # puts "------------------------------------------------------"
-#   # puts key["Title"]
-#   # puts "------------------------------------------------------"
-#   # puts key["Table"]
-# end
-
 def eliminate_headers(json_obj)
   vote_record = json_obj["TaggedPDF-doc"]["Part"]
   vote_record[1..-1]
@@ -31,15 +22,11 @@ def grab_voted_on_legislation(json_obj)
   json_obj["TaggedPDF-doc"]
 end
 
-# votes1["TaggedPDF-doc"]["Part"][1]["Table"]["TR"][1]["TD"]  #=> contains the Title of Legislation and City-Identifier
-
-
-# THIS SEEMS TO WORK FOR VOTES 1, 2 #############################################
+# THIS SEEMS TO WORK FOR VOTES 1, 2, 3, 4, (NOT 5 or 6) #########################
 #################################################################################
 #################################################################################
 
 def get_legislation_title_and_city_identifier(json)
-  # THIS METHOD WILL HOUSE SOME COOL SHIT HOPEFULLY
   legislation_info = []
   table_length = json["TaggedPDF-doc"]["Part"][1]["Table"].length
   if table_length > 1
@@ -58,15 +45,6 @@ def get_legislation_title_and_city_identifier(json)
   legislation_info
 end
 
-# p get_legislation_title_and_city_identifier(votes1)
-# p get_legislation_title_and_city_identifier(votes2)
-# p get_legislation_title_and_city_identifier(votes3).length
-# p get_legislation_title_and_city_identifier(votes4).length
-# p get_legislation_title_and_city_identifier(votes5).length
-p get_legislation_title_and_city_identifier(votes6).length
-
-
-
 
 
 # THIS GETS US THE VOTES ---------------------------------------
@@ -77,12 +55,52 @@ def get_vote_tallies(json_votes)
 
     if key["Sect"].class == Array    # DOCUMENT WITH MULTIPLE ORDINANCES TO VOTE ON
       key["Sect"].each do |hash|
+      # THIS ISN'T WORKING NOW>>>>>>>
+      #   if hash != nil && hash.has_key?("Sect")
+      #     votes = []
+      #     puts "Yep."
+      #     hash["Sect"].each do |nested_sect|
+      #       if nested_sect.class == Hash && nested_sect.has_key?("Table")
+      #         nested_sect.each do |table_hash|
+      #           table_hash.each do |key, value|
+      #             # puts "THIS #{key} : #{value}"
+      #             if key.class == Hash
+      #               # puts key["TR"]
+      #               # puts value["TR"]
+      #               key["TR"].each do |alderman_hash|
+      #                 if alderman_hash["TD"]
+      #                   votes << alderman_hash["TD"]
+      #                   puts votes.length
+      #                 end
+
+      #               end
+      #               value["TR"].each do |alderman_hash|
+      #                 if alderman_hash["TD"]
+      #                   # p alderman_hash["TD"]
+      #                   votes << alderman_hash["TD"]
+      #                   puts votes.length
+      #                 end
+
+      #               end
+      #               puts "+++++++++++++++++++++++++++++++++++++++"
+      #             end
+      #           end
+      #         # votes.clear
+
+      #         end
+      #       end
+      #     end
+      #     votes_on_pending_legislation << votes
+      #     votes_on_pending_legislation.each { |x|
+      #       puts "__________________________________"
+      #       p x
+      #     }
+      #   end
         if hash != nil && hash.has_key?("Table")
           votes = []
           hash['Table'].flatten.length
           hash["Table"].each do |table_hash|
             table_hash.each do |key, value|
-              # puts "THIS #{key} : #{value}"
               value.each do |alderman_hash|
                 # if alderman_hash["TH"]
                 #   puts "This is a header"
@@ -96,6 +114,7 @@ def get_vote_tallies(json_votes)
             end
           end
           votes_on_pending_legislation << votes
+          puts votes_on_pending_legislation.length
         elsif hash != nil && hash.has_key?("Sect")   # DOCUMENT WITH A SINGLE ORDINANCE TO VOTE ON
           votes = []
           hash["Sect"][1].each do |table_hash|
@@ -117,12 +136,28 @@ def get_vote_tallies(json_votes)
         end
       end
     end
+    if !votes_on_pending_legislation.empty?
     puts "COMPARE LENGTH OF LEGISLATION INFO TO VOTE TALLIES"
     p votes_on_pending_legislation.length
+    end
   end
 end
 
+# p get_legislation_title_and_city_identifier(votes1).length
+# get_vote_tallies(votes1)
+# p get_legislation_title_and_city_identifier(votes2).length
+# get_vote_tallies(votes2)
+# p get_legislation_title_and_city_identifier(votes3).length
+# get_vote_tallies(votes3)
+# p get_legislation_title_and_city_identifier(votes4).length
+# get_vote_tallies(votes4)
+# get_vote_tallies(votes5)
+# p get_legislation_title_and_city_identifier(votes6).length
+# get_vote_tallies(votes6)
 
+get_vote_tallies(votes5)
+puts "====================================="
+p get_legislation_title_and_city_identifier(votes5).length
 
 
 
