1551d81ff3a034f6ec95e04f26f40acdfe43c6a0
Justin Stewart <email": null>
2014-06-16 01:09:07 -0500

    This algo is almost complete

--- parser.rb
+++ parser.rb
@@ -22,17 +22,73 @@ def grab_voted_on_legislation(json_obj)
   json_obj["TaggedPDF-doc"]
 end
 
-votes1["TaggedPDF-doc"]["Part"][1]["Table"]["TR"][1]["TD"]  #=> contains the Title of Legislation and City-Identifier
+# votes1["TaggedPDF-doc"]["Part"][1]["Table"]["TR"][1]["TD"]  #=> contains the Title of Legislation and City-Identifier
 
 
-votes2.flatten[1]["Part"].each do |key, value|
-  # puts key
-  puts "========================="
-  puts key["Sect"].flatten
-  puts "========================="
+# THIS SEEMS TO WORK FOR VOTES 1, 2
+
+votes1["TaggedPDF-doc"]["Part"][1]["Table"]["TR"].each do |header_hash|
+  if header_hash.has_key?("TD")
+    puts header_hash["TD"]  # Legislation Info
+    puts "======================================================"
+  end
 end
 
-puts "=============================================================="
+
+
+# THIS GETS US THE VOTES ---------------------------------------
+
+votes1.flatten[1]["Part"].each do |key, value|
+
+  if key["Sect"].class == Array
+    key["Sect"].each do |hash|
+      if hash != nil && hash.has_key?("Table")
+        hash["Table"].each do |table_hash|
+          table_hash.each do |key, value|
+            # puts "THIS #{key} : #{value}"
+            value.each do |alderman_hash|
+              p alderman_hash.values.flatten
+              puts "-------------------------------------------------------"
+            end
+          end
+        end
+      elsif hash.has_key?("Sect")
+
+        hash["Sect"][1].each do |table_hash|
+
+          table_hash.flatten.each do |vote_hash|
+
+          end
+        end
+
+
+
+      end
+    end
+  end
+end
+
+
+puts "-------------------------------------------------------"
+puts "-------------------------------------------------------"
+puts "-------------------------------------------------------"
+
+# votes1.flatten[1]["Part"].each do |key, value|
+#   # puts "========================="
+#   # puts key["Sect"]
+#   # puts "========================="
+#   # puts value
+#   puts "=============================================================="
+#   if key["Sect"].class == Array
+#     puts "yea"
+#     key["Sect"].each do |hash|
+#       puts "++++++++++++++++++++++++++++++++++++++"
+#       puts hash
+#       puts "++++++++++++++++++++++++++++++++++++++"
+#     end
+#   end
+# end
+
 
 # votes1.flatten[1]["Part"].each do |key, value|   #IT HAS NO VALUE ========     THIS IS FUCKED
 #   puts key
